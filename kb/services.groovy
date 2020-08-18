ba9a7f850d3abd3f1dc5f5b3d869db5d0fd6e375
Marcin Pas <marcin.pas@softelnet.com>
2020-08-18 17:45:25 +0200

    Changes.

---
+++ kb/services.groovy
@@ -0,0 +1,11 @@
+class HelloFromGitGroovy extends Action {
+    void onConfigure() {
+        this.withLabel("Hello from git - Groovy").withDescription("Action defined in the git knowledge base.")
+        this.withNoArgs()
+        this.withResult(new StringType().withLabel("Hello")))
+    }
+
+    String onCall() {
+        return "Hello from the Groovy git knowledge base"
+    }
+}
