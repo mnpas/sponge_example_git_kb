b6f3233577efb9d2bc2df9e2043f72c141f0c340
Marcin Pas <marcin.pas@softelnet.com>
2020-08-18 19:08:23 +0200

    Changes.

--- kb/services.groovy
+++ kb/services.groovy
@@ -11,19 +11,6 @@ class HelloWorldActionGroovy extends Action {
     }
 }
 
-class HelloWorldActionGroovy extends Action {
-    void onConfigure() {
-        this.withLabel("Hello world - Groovy").withDescription("Returns a greeting text.")
-        this.withArg(new StringType("name").withLabel("Your name").withDescription("Type your name."))
-        this.withResult(new StringType().withLabel("Greeting").withDescription("The greeting text."))
-        this.withFeature("icon", "git")
-    }
-
-    String onCall(String name) {
-        return "Hello World! Hello $name! 3333"
-    }
-}
-
 // Required by Groovy.
 void onLoad() {
 }
