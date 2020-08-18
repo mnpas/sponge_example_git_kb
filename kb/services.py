e7831a5ee445886de50de8f4b7ee1c424bf87f4d
Marcin Pas <marcin.pas@softelnet.com>
2020-08-18 17:05:30 +0200

    Changes.

--- kb/services.py
+++ kb/services.py
@@ -1,8 +1,18 @@
 
+def onBeforeLoad():
+    sponge.disable(HelloFromGit)
+
+
+def onAfterLoad():
+    pass
+
+
 class HelloFromGit(Action):
     def onConfigure(self):
-        self.withLabel("Hello from git").withDescription("Action defined in the git knowledge base.")
+        self.withLabel("Hello from git").withDescription(
+            "Action defined in the git knowledge base.")
         self.withNoArgs().withResult(StringType().withLabel("Hello"))
         self.withFeature("icon", "git")
+
     def onCall(self):
-        return "Hello from the git knowledge base 2"
+        return "Hello from the git knowledge base " + sponge.version
