26c051eb946b5b5eb6d996373545f0856d87d381
Marcin Pas <marcin.pas@softelnet.com>
2020-08-24 15:06:38 +0200

    Changes.

---
+++ kb/my_service.py
@@ -0,0 +1,7 @@
+class OsGetDiskSpaceInfo(Action):
+    def onConfigure(self):
+        self.withLabel("Get disk space info").withDescription("Returns the disk space info.")
+        self.withNoArgs().withResult(StringType().withFormat("console").withLabel("Disk space info"))
+        self.withFeature("icon", "console")
+    def onCall(self):
+        return sponge.process("df", "-h").outputAsString().run().outputString
